11a259e92cf808925eeee59f0bfdb146db90c8b6
Roch Dionnet <roch.dionnet@gmail.com>
2016-06-21 20:41:15 +0200

    First commit

---
+++ src/org/neotouch/neopad/Builder.java
@@ -0,0 +1,6 @@
+package org.neotouch.neopad;
+
+public interface Builder<T>
+{
+	public T build();
+}
---
+++ src/org/neotouch/neopad/Neopad.java
@@ -0,0 +1,38 @@
+package org.neotouch.neopad;
+
+import java.io.IOException;
+
+import javax.sound.midi.MidiUnavailableException;
+
+import org.neotouch.neopad.model.GuiLaunchpadModel;
+import org.neotouch.neopad.model.LaunchpadDeviceModel;
+import org.neotouch.neopad.model.SequenceModel;
+import org.neotouch.neopad.view.LaunchpadDevice;
+import org.neotouch.neopad.view.NeopadGui;
+
+public class Neopad
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		LaunchpadDeviceModel deviceModel = new LaunchpadDeviceModel();
+		@SuppressWarnings("resource")
+		LaunchpadDevice deviceView = new LaunchpadDevice();
+		deviceView.addModel(deviceModel);
+		try {
+			deviceView.makeUserChooseDevices();
+		} catch (MidiUnavailableException e) {
+			e.printStackTrace();
+		}
+
+		GuiLaunchpadModel guiModel = new GuiLaunchpadModel();
+		SequenceModel seqModel = new SequenceModel();
+		seqModel.addFrame(0, 0, 0);
+		seqModel.addFrame(0, 0, 0);
+		NeopadGui guiView = new NeopadGui();
+		guiView.addModel(guiModel);
+		guiView.addModel(deviceModel);
+		guiView.addModel(seqModel);
+	}
+
+}
